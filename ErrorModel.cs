1655b7ef18a23c7a745441475bab362ca9f37e49
CurtisHolton1 <curtisholton1@outlook.com>
2015-09-22 21:27:26 -0400

    initial

---
+++ ErrorModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ErrorLogger
+{
+    public class ErrorModel
+    {
+        public enum SeverityEnum
+        {
+            [XmlEnum(Name="Warning")]
+           Warning,
+            [XmlEnum(Name = "Alert")]
+           Alert,
+            [XmlEnum(Name = "Error")]
+           Error
+        }
+
+        public SeverityEnum Severity { get; set; }
+        public string AppName { get; set; }
+        public DateTime Time { get; set; }
+        public string MethodName { get; set; }
+        public string ExceptionDetails { get; set; }
+        public string Message { get; set; }
+
+        public ErrorModel(Exception e, string appName, string methodName, SeverityEnum severity, string message = "")
+        {
+            this.Time = DateTime.Now;
+            this.Severity = severity;
+            this.AppName = appName;
+            this.MethodName = methodName;
+            this.ExceptionDetails = GetExceptionDetails(e);
+            this.Message = message;
+        }
+        
+        private ErrorModel()
+        {
+            //needed for serialization
+        }
+        private string GetExceptionDetails(Exception exception)
+        {
+            if (exception == null)
+            {
+                return "";
+            }
+            try
+            {
+                var properties = exception.GetType().GetProperties();
+                var fields = properties
+                              .Select(property => new
+                              {
+                                  Name = property.Name,
+                                  Value = property.GetValue(exception, null)
+                              })
+                              .Select(x => String.Format(
+                                  "{0} = {1}",
+                                  x.Name,
+                                  x.Value != null ? x.Value.ToString() : String.Empty
+                              ));
+                return String.Join("\n", fields);
+
+            }
+            catch (Exception e)
+            {
+                
+            }
+            return "";
+        }
+    }
+}
---
+++ XmlErrorWriter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Web;
+using System.Xml.Serialization;
+using ErrorLogger;
+
+namespace ErrorLogger
+{
+     
+    public static class XmlErrorWriter
+    {   
+        private static List<ErrorModel> _errors;
+        private static string SaveLocation { get; set; }
+
+        static XmlErrorWriter()
+        {
+            _errors = new List<ErrorModel>();
+        }
+        public static string AddError(ErrorModel model, string saveLocation = "Errors.xml")
+        {
+            SaveLocation = saveLocation;
+            _errors = Deserialize();
+            _errors.Add(model);     
+            SerializeAndSave();    
+
+            return SaveLocation;
+        }
+        private static void SerializeAndSave()
+        {
+            try
+            {
+                    var serializer = new XmlSerializer(typeof(List<ErrorModel>));
+                    TextWriter writer = new StreamWriter(SaveLocation);
+                    serializer.Serialize(writer,_errors);
+                    writer.Close();
+                    writer.Dispose();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public static List<ErrorModel> Deserialize(string saveLocation = "Errors.xml")
+        {
+            SaveLocation = saveLocation;
+            SaveLocation = HttpContext.Current.Server.MapPath(saveLocation);
+
+            var deserialized = new List<ErrorModel>();
+            try
+            {
+                var serializer = new XmlSerializer(typeof(List<ErrorModel>));
+                var reader = new StreamReader(SaveLocation);
+                deserialized = (List<ErrorModel>)serializer.Deserialize(reader);
+                reader.Close();
+
+                if (deserialized == null)
+                    deserialized = new List<ErrorModel>();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return deserialized;
+
+        }
+    
+    }
+}
